ece398d22c420e17b7eaf3080479dbced1ae8169
Viswanadh Alubilli <41397984+visu84@users.noreply.github.com>
2020-03-25 13:30:46 -0400

    Add files via upload

---
+++ list_eip.py
@@ -0,0 +1,18 @@
+#This function will list all EIPs from all region for this account
+#Note--- Please change the default timout period for the lambda function to atleast 3 mins
+
+import json
+import json
+import boto3
+
+session = boto3.Session()
+client = session.client('ec2')
+
+def lambda_handler(event, context):
+    for region_dict in client.describe_regions()['Regions']:
+        region = region_dict['RegionName']
+        print('Region', region)
+        client_region = session.client('ec2', region)
+        addresses_dict = client_region.describe_addresses()
+        for eip_dict in addresses_dict['Addresses']:
+            print(eip_dict['PublicIp'])
